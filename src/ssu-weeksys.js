d343e4ab0bd5074b14685a2942760d01be32ced7
Artem Shevchenko <shevchenkoaa@sgu.ru>
2016-11-17 22:09:43 +0300

    Minified

--- src/ssu-weeksys.js
+++ src/ssu-weeksys.js
@@ -1,15 +1,10 @@
 var getNumerator = function(){
     var dateNow = new Date(),
-        year = dateNow.getMonth() < 9 ? dateNow.getFullYear() - 1 : dateNow.getFullYear(),
+        year = dateNow.getMonth() < 8 ? dateNow.getFullYear() - 1 : dateNow.getFullYear(),
         dateFrom  = new Date(year, 8, 1),
         weekNumber = Math.round((dateNow - dateFrom) / (86400000 * 7));
 
-      if(weekNumber % 2){
-        return "denominator";
-      }else{
-        return "numerator";
-      }
+      return weekNumber % 2 ? "знаменатель" : "числитель";
 };
 
 $("#ssu-weeksys").html(getNumerator());
-
