6a9fca1a397f2b57520bc2c541d978a419e25a59
Artem Shevchenko <shevchenkoaa@sgu.ru>
2016-11-17 19:45:27 +0300

    Minified

--- js/ssu-weeksys.js
+++ js/ssu-weeksys.js
@@ -1,1 +1,1 @@
-var getNumerator=function(){var a=new Date,b=a.getMonth()<9?a.getFullYear()-1:a.getFullYear(),c=new Date(b,8,1),d=Math.round((a-c)/6048e5);return d%2?"знаменатель":"числитель"};$("#ssu-weeksys").html(getNumerator());
+var getNumerator=function(){var a=new Date,b=a.getMonth()<8?a.getFullYear()-1:a.getFullYear(),c=new Date(b,8,1),d=Math.round((a-c)/6048e5);return d%2?"знаменатель":"числитель"};$("#ssu-weeksys").html(getNumerator());
